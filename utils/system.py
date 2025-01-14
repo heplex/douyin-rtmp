e06b2b341290ddb168036478abe5855d76b11aa1
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2025-01-14 09:59:29 +0800

    feat: 修改获取程序运行路径的方法，使其兼容nuitka
    Former-commit-id: 23eaf021bb69cd512b07f6e8a24c71105ac2b9d6

--- utils/system.py
+++ utils/system.py
@@ -10,12 +10,12 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin() != 0
 
 def get_resource_path(relative_path):
-    """获取资源文件的绝对路径，支持开发环境和打包后的环境"""
+    """获取资源文件的绝对路径，兼容开发、Nuitka打包和PyInstaller打包后的环境"""
     if hasattr(sys, '_MEIPASS'):
-        # PyInstaller 打包后的路径
+        # 在PyInstaller打包后的环境中
         base_path = sys._MEIPASS
     else:
-        # 开发环境下的路径
-        base_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    
-    return os.path.join(base_path, relative_path) 
+        # 在Nuitka打包后的环境或开发环境中
+        base_path = os.path.dirname(os.path.dirname(__file__))
+
+    return os.path.join(base_path, relative_path)
